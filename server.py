c7697caa34f4bf1c2a3f50a17b2e1830cf6a7bd0
Harshul <harshulgupta811@gmail.com>
2025-03-22 21:29:29 -0400

    Updated scoreboard to support real-time updates

--- server.py
+++ server.py
@@ -38,9 +38,10 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
+    sorted_scoreboard = sorted(scoreboard, key=lambda x: x["score"], reverse=True)
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
-@app.route('/increase_score', methods=['GET', 'POST'])
+@app.route('/increase_score', methods=['POST'])
 def increase_score():
     global scoreboard
 
@@ -50,7 +51,9 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+            break
+    
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
